28088f7cc8d630045ddc256036733a8a78e6a862
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-07-14 20:53:59 -0300

    AsyncTask1

---
+++ AsyncTask1/app/src/main/java/samuel/jose/asynctask1/MainActivity.java
@@ -0,0 +1,23 @@
+package samuel.jose.asynctask1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progressBar = findViewById(R.id.progressBar);
+    }
+
+    public void startAsyncTask(View view) {
+        MyAsyncTask task = new MyAsyncTask(progressBar, MainActivity.this);
+        task.execute(5);
+    }
+}
