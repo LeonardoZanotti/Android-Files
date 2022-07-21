bb785a0cc7b047eaf43441be2b2b6cf280b21518
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-07-21 19:27:46 -0300

    list of items

--- ToDoList/app/src/main/java/samuel/jose/todolist/MainActivity.java
+++ ToDoList/app/src/main/java/samuel/jose/todolist/MainActivity.java
@@ -2,18 +2,55 @@ package samuel.jose.todolist;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ToDoAdapter toDoAdapter;
+    private List<ToDo> toDoList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerViewToDoList);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        instantiateRecyclerToDo();
+    }
+
+    private void instantiateRecyclerToDo() {
+        ToDo toDo1 = new ToDo();
+        toDo1.setToDoName("Go shopping");
+        toDoList.add(toDo1);
+
+        ToDo toDo2 = new ToDo();
+        toDo2.setToDoName("Read an android article");
+        toDoList.add(toDo2);
+
+        toDoAdapter = new ToDoAdapter(toDoList);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext(), LinearLayout.VERTICAL));
+        recyclerView.setAdapter(toDoAdapter);
     }
 
     @Override
@@ -25,7 +62,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == R.id.menu_add) {
-            Toast.makeText(this, "click", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(getApplicationContext(), AddToDoActivity.class);
+            startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
     }
