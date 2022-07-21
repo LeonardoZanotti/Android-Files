7ea2863afb1445676afe459dba3db673e0f765ab
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-07-21 18:20:38 -0300

    added menu

--- ToDoList/app/src/main/java/samuel/jose/todolist/MainActivity.java
+++ ToDoList/app/src/main/java/samuel/jose/todolist/MainActivity.java
@@ -1,8 +1,12 @@
 package samuel.jose.todolist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_add) {
+            Toast.makeText(this, "click", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
