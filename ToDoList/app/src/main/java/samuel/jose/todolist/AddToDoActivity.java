c38631c6247aad47fff19e3ac859feb741181765
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-07-21 19:40:33 -0300

    all functions done in theory

--- ToDoList/app/src/main/java/samuel/jose/todolist/AddToDoActivity.java
+++ ToDoList/app/src/main/java/samuel/jose/todolist/AddToDoActivity.java
@@ -1,8 +1,12 @@
 package samuel.jose.todolist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class AddToDoActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class AddToDoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_to_do);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_add_to_do, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.itemSalvar) {
+            Toast.makeText(this, "Save", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
